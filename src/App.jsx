1c4fdbf406628472e79e1bfd4563377767bfb5ec
Ana <anawu.datawork@gmail.com>
2025-01-15 22:25:46 +0800

    新增React DataTable與React Leaflet套件

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className='flex flex-col md:flex-row'>
+      <div className='bg-red-200 p-2'>Lorem ipsum dolor sit amet.</div>
+      <div className='bg-blue-200 p-2'>Lorem ipsum dolor sit amet.</div>
+      <div className='bg-green-200 p-2'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ut.
+      </div>
+      <div className='bg-orange-200 p-2'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+        officiis!
+      </div>
+    </div>
+  );
 }
 
 export default App;
